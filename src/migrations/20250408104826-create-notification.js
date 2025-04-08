938115fda95266fa0eeb840fa5c91f7b47384b11
Abhrajit Chatterjee <abhrajitc23@gmail.com>
2025-04-08 16:27:54 +0530

    Notifications model created

---
+++ src/migrations/20250408104826-create-notification.js
@@ -0,0 +1,50 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Notifications', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      subject: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      content: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      recipientEmail: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        validate: {
+          isEmail: true
+        }
+      },
+      status: {
+        type: Sequelize.ENUM,
+        allowNull: false,
+        values: ["PENDING", "FAILED", "SUCCESS"],
+        defaultValue: "PENDING"
+      },
+      notificationTime: {
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Notifications');
+  }
+};
---
+++ src/models/notification.js
@@ -0,0 +1,47 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Notification extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Notification.init({
+    subject: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    content: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    recipientEmail: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        isEmail: true,
+      }
+    },
+    status: {
+      type: DataTypes.ENUM,
+      allowNull: false,
+      values: ["PENDING", "SUCCESS", "FAILED"],
+      defaultValue: "PENDING"
+    },
+    notificationTime: {
+      type: DataTypes.DATE,
+      allowNull: false
+    }
+  }, {
+    sequelize,
+    modelName: 'Notification',
+  });
+  return Notification;
+};
